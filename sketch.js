8d701a57c04b085b88e64033f5ebd14e7ddb9aad
Skexic <89268936+Skexic@users.noreply.github.com>
2021-08-31 13:49:55 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var seaImg
+var shipImg 
+var sea,ship
+
+
+
+function preload(){
+ 
+  seaImg = loadImage("sea.png")
+  shipImg = loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png")
+  
+
+
+}
+
+function setup(){
+  createCanvas(400,400);
+  
+  sea=createSprite(400,200)
+  sea.addImage(seaImg)
+  sea.scale=0.3
+
+
+  ship=createSprite(130,200,30,30)
+  ship.addAnimation("moving",shipImg)
+  ship.scale=0.25
+}
+
+function draw() {
+  background("blue");
+
+  sea.velocityX=-2
+
+  if(sea.x< 0){
+    sea.x=sea.width/8
+  }
+
+drawSprites()
+
+}
